f8ddc5ff30926a70e620760bb5a6209a17bf0de2
pierrevaneck <oomdikdaan@gmail.com>
2015-10-16 10:28:39 -0500

    Spatial Query

---
+++ SpatialQuery.sql
@@ -0,0 +1,16 @@
+use EDFacts_Tableau
+
+; with cte as (
+select top 100 sch.File_Record_Number RecNum, 
+	sch.School_Address_Location_1 + ',' + sch.School_City_Location + ',' + sch.School_USPS_State_Abbreviation_Location+ ',' + sch.School_ZIP_Code_Location Physical_Address,
+	sch.School_City_Location + ',' + sch.School_USPS_State_Abbreviation_Location+ ',' + sch.School_ZIP_Code_Location Physical_Address_City
+from dbo.S029_SCH sch
+where sch.Latitude is null
+)
+update dbo.S029_SCH
+set Latitude = d.Latitude, Longitude=d.Longtitude, SpatialPoint=d.SpatialPoint
+--select c.*, d.*
+from cte c
+	inner join dbo.S029_SCH sch on c.RecNum=sch.File_Record_Number
+	cross apply dbo.fn_GetLongLat(c.Physical_Address) d 
+	
